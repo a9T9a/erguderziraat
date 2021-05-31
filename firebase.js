415f13a2b27e41d39c0f85e2ae87451b498f5c63
a9T9a <71823597+a9T9a@users.noreply.github.com>
2021-05-31 17:43:49 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,13 +2,7 @@ import firebase from "firebase/app"
 import "firebase/firebase-firestore.memory"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtqLWgpfGA7wC9wEesatP8ltNoTg1-HH0",
-    authDomain: "erguderzm-e0e93.firebaseapp.com",
-    projectId: "erguderzm-e0e93",
-    storageBucket: "erguderzm-e0e93.appspot.com",
-    messagingSenderId: "53669379620",
-    appId: "1:53669379620:web:8f20c83148b58cb89be2b1",
-    measurementId: "G-BS49JJYZJW"
+
 }
 
 firebase.initializeApp(firebaseConfig)
